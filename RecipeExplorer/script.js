62e9a92c4bb31f8c562b87f6783f0421a7c4a4df
malalatyna <raherinirina.malalatiana@gmail.com>
2023-06-27 18:03:10 +0400

    2nd commit

--- RecipeExplorer/script.js
+++ RecipeExplorer/script.js
@@ -1,8 +1,15 @@
-
+ // Retrieve the necessary elements
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
 const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
+//form-add-recipe
+const addRecipesLink = document.getElementById('addRecipesLink');
+const formElement = document.querySelector('.form-content');
+const formCloseBtn = document.getElementById('form-close-btn');
+//upload recipe
+const addRecipesButton = document.getElementById('add-recipe-button');
+const formDataElement = document.getElementById('form-data');
 
 // Event listeners
 searchBtn.addEventListener('click', getMealList);
@@ -59,19 +66,17 @@ function mealRecipeModal(meal) {
   const mealItem = meal[0];
   const html = `
     <h2 class="recipe-title">${mealItem.strMeal}</h2>
-    <p class="recipe-category">${mealItem.strCategory}</p>
-    <div class="recipe-instruct">
-      <h3>Instructions:</h3>
-      <p>${mealItem.strInstructions}</p>
-    </div>
     <div class="recipe-meal-img">
       <img src="${mealItem.strMealThumb}" alt="">
     </div>
     <div class="recipe-link">
-      <a href="${mealItem.strYoutube}" target="_blank">Watch Video</a>
+    <a href="${mealItem.strYoutube}" target="_blank"><i class="fab fa-youtube"></i></i></a>
+  </div>
+       <div class="recipe-instruct">
+      <h3>Instructions:</h3>
+      <p>${mealItem.strInstructions}</p>
     </div>
   `;
-  
   mealDetailsContent.innerHTML = html;
   mealDetailsContent.parentElement.classList.add('showRecipe');
 }
@@ -80,3 +85,54 @@ function mealRecipeModal(meal) {
 function closeRecipeModal() {
   mealDetailsContent.parentElement.classList.remove('showRecipe');
 }
+
+ // Add an event listener to the "Add Recipes" link
+ addRecipesLink.addEventListener('click', function(event) {
+   event.preventDefault(); 
+   formElement.style.display = 'block';
+ });
+
+ // Add an event listener to the close button
+ formCloseBtn.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  formElement.style.display = 'none';
+});
+
+//upload the recipe
+
+addRecipesButton.addEventListener('click', function(event) {
+  event.preventDefault();
+
+  // Retrieve the form inputs
+  const nameInput = document.getElementById('name');
+  const titleInput = document.getElementById('title');
+  const ingredientsInput = document.getElementById('ingredients');
+  const contactInput = document.getElementById('contact');
+  const descriptionInput = document.getElementById('description');
+
+  // Get the values of the form inputs
+  const name = nameInput.value;
+  const title = titleInput.value;
+  const ingredients = ingredientsInput.value;
+  const contact = contactInput.value;
+  const description = descriptionInput.value;
+
+  // Display the form data below the form
+  const formDataHTML = `
+    <h3>Your recipe details:</h3>
+    <p><strong>Name:</strong> ${name}</p>
+    <p><strong>Title:</strong> ${title}</p>
+    <p><strong>Ingredients:</strong> ${ingredients}</p>
+    <p><strong>Contact Number:</strong> ${contact}</p>
+    <p><strong>Description:</strong> ${description}</p>
+  `;
+
+  formDataElement.innerHTML = formDataHTML;
+
+  // Clear the form inputs
+  nameInput.value = '';
+  titleInput.value = '';
+  ingredientsInput.value = '';
+  contactInput.value = '';
+  descriptionInput.value = '';
+});
